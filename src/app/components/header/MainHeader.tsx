278900b95eef5aaeb982923a964a26260d1b27d7
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2023-11-04 16:47:03 +0100

    Started on mainpage and color profiles

---
+++ src/app/components/header/MainHeader.tsx
@@ -0,0 +1,29 @@
+'use client'
+import Link from 'next/link'
+
+export default function MainHeader() {
+    return (
+
+        <nav className='w-screen h-fit overflow-hidden bg-header dark:bg-header_d'>
+            <div className="lg:text-2xl sm:text-lg text-sm py-4 lg:px-8 px-4 w-full sm:w-10/12 h-16 m-auto flex items-center justify-between">
+                <div>
+                <Link href={"#"} className="lg:text-4xlxl sm:text-2xl text-xl font-extrabold">
+                        EDV
+                    </Link>
+                </div>
+                <div className='flex lg:gap-8 gap-6 uppercase items-center'>
+                    <Link href={"#"}>
+                        About Me
+                    </Link>
+                    <Link href={"#"} >
+                        Projects
+                    </Link>
+                    <Link href={"#"} >
+                        Contact
+                    </Link>
+
+                </div>
+            </div>
+        </nav>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,24 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      //header
+      'header_d' : '#1b1f2a',
+      'header': '#d4d4d4',
+
+      //background
+      'background_d' : '#0d0f14',
+      'background' : '#fafafa',
+
+      //box
+
+
+      //background
+      'popcolor' : '#F54B64',
+      'light_grey': '#4e596f',
+      'lighter_grey': '#c4c8d3',
+
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
